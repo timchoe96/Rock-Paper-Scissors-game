4205da12f9f2f6c96da27c6a6dcdaeb91411591f
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-07-11 21:21:17 -0400

    Updates

---
+++ precache-manifest.3b7b9da8ad89cac5b3b8d211a853ed78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96faef40e3b79120c3bce8d7424825b2",
+    "url": "/Rock-Paper-Scissors-game/index.html"
+  },
+  {
+    "revision": "8bbdd344d873780189b1",
+    "url": "/Rock-Paper-Scissors-game/static/js/2.546b3936.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rock-Paper-Scissors-game/static/js/2.546b3936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c38a851ac16e37bb9c",
+    "url": "/Rock-Paper-Scissors-game/static/js/main.52e60b3d.chunk.js"
+  },
+  {
+    "revision": "9d4c1aebf175315a6f67",
+    "url": "/Rock-Paper-Scissors-game/static/js/runtime-main.80a0d5cd.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/Rock-Paper-Scissors-game/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/Rock-Paper-Scissors-game/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/Rock-Paper-Scissors-game/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/Rock-Paper-Scissors-game/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/Rock-Paper-Scissors-game/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/Rock-Paper-Scissors-game/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/Rock-Paper-Scissors-game/static/media/logo.759a2075.svg"
+  }
+]);
